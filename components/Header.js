265ee2cafe6b6af2b3da54b54ff8f9f3703b89c4
Konspiracije <konspiracijex@gmail.com>
2025-12-11 22:17:13 +0100

    Update Header.js

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>MysteryFiles Portal</h1>
+      <nav>
+        <a href="/">Home</a> | <a href="/about">About</a>
+      </nav>
+    </header>
+  );
+}
