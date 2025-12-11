79735a25cbdcde5d38df2343fbcb5ebd5a23f6b9
Konspiracije <konspiracijex@gmail.com>
2025-12-11 22:18:26 +0100

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>About MysteryFiles</h2>
+        <p>This is a bilingual investigative portal dedicated to mysteries and hidden truths.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
